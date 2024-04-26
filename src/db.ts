5bcb8aafed29ea3f15bf34dced507755d41ee557
Alex Taboada <alexandrotaboada@gmail.com>
2024-04-26 11:42:04 -0300

    use config variables

--- src/db.ts
+++ src/db.ts
@@ -1,14 +1,13 @@
 import { DataSource, DataSourceOptions } from "typeorm"
-import dotenv from 'dotenv'
-dotenv.config();
+import { environment } from "./config";
 
 export const conObject:DataSourceOptions = {
     type: "postgres",
-    host: process.env.POSTGRES_HOST,
-    port: Number(process.env.POSTGRES_PORT),
-    username: process.env.POSTGRES_USERNAME,
-    password: process.env.POSTGRES_PASS,
-    database: process.env.POSTGRES_DB,
+    host: environment.database.host,
+    port: Number(environment.database.port),
+    username: environment.database.username,
+    password: environment.database.password,
+    database: environment.database.db,
     synchronize:true,
     entities:[/*Base, Transaction, User, BankAccount, ResetPassword, Account*/],
 }
