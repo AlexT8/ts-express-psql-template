78811ddae8b52f898eaa1caf0d9abd523e9bdab9
Alex Taboada <alexandrotaboada@gmail.com>
2024-04-26 11:19:23 -0300

    added environment validation

---
+++ src/config/index.ts
@@ -0,0 +1,41 @@
+import dotenv from 'dotenv'
+import Joi from 'joi'
+import { HttpException } from '../exceptions/HttpException'
+dotenv.config()
+
+const { env } = process
+
+export const config = {
+  urls: {
+    app:env.APP_URL 
+  },
+  database: {
+    db: env.POSTGRES_DB,
+    username: env.POSTGRES_USERNAME,
+    password: env.POSTGRES_PASS,
+    port: env.POSTGRES_PORT,
+    host: env.POSTGRES_HOST
+  }
+}
+
+export const schema = Joi.object({
+  urls: {
+    app: Joi.string().required()
+  },
+  database: {
+    db: Joi.string().required(),
+    username: Joi.string().required(),
+    password: Joi.string().required(),
+    port: Joi.string().required(),
+    host: Joi.string().required()
+  }
+})
+
+
+export const validateEnvironment = async () => {
+  try {
+    await schema.validateAsync(config)
+  } catch (error: any) {
+    if(error?.details?.length > 0) throw new HttpException(400, 'Invalid .env variables', error)
+  }
+}
